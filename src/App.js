25445526ea644ff3e6defb86eb05b9066c1b5ecb
nabilaaini2510 <nabilaaini18@gmail.com>
2022-06-25 09:36:13 +0700

    update css and icon

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
 import Todo from "./Components/Todo";
-
+import BottomNav from "./Layout/Footer";
 function App() {
   return (
-    <div className="container">
-      <Todo />
-    </div>
+    <>
+      <div className="container">
+        <Todo />
+      </div>
+      <BottomNav />
+    </>
   );
 }
 
---
+++ src/Layout/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const BottomNav = () => {
+  return (
+    <div class="footer">
+      <p>&copy; 2022 Nabs</p>
+    </div>
+  );
+};
+
+export default BottomNav;
