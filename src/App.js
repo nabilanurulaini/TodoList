c8bc177cb8ca33e24093819df09c36a2983cfc59
nabilaaini2510 <nabilaaini18@gmail.com>
2022-06-25 08:15:43 +0700

    add react todo list app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Todo from "./Components/Todo";
+
+function App() {
+  return (
+    <div className="container">
+      <Todo />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/TodoForm.js
@@ -0,0 +1,67 @@
+import React, { useState, useEffect, useRef } from "react";
+import { MdAdd } from "react-icons/md";
+
+const TodoForm = (props) => {
+  const [input, setInput] = useState(props.edit ? props.edit.value : "");
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    inputRef.current.focus();
+  });
+
+  const handleChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const dt = props.edit ? props.edit.id : new Date().getTime();
+
+    props.onSubmit({
+      id: dt,
+      text: input,
+    });
+    setInput("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="todo-input-form">
+        {props.edit ? (
+          <>
+            <input
+              placeholder="Update todo"
+              value={input}
+              onChange={handleChange}
+              name="text"
+              ref={inputRef}
+              className="todo-input-edit"
+              maxLength={300}
+              autoComplete="off"
+            />
+          </>
+        ) : (
+          <>
+            <input
+              placeholder="Add an activity"
+              value={input}
+              onChange={handleChange}
+              name="text"
+              className="todo-input"
+              ref={inputRef}
+              maxLength={300}
+              autoComplete="off"
+            />
+            <MdAdd
+              size={45}
+              onClick={handleSubmit}
+              className="delete-icon"
+            />
+          </>
+        )}
+      </div>
+    </form>
+  );
+};
+
+export default TodoForm;
